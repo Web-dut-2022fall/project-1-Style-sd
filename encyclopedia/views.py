f53a779b282c20e41c44cefbddfa1300b201979f
Style-sd <117788454+Style-sd@users.noreply.github.com>
2022-12-06 13:51:49 +0800

    Add files via upload

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from markdown2 import Markdown
 
 from . import util
 
@@ -8,3 +9,20 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def title(request, title):
+    wiki_page = util.get_entry(title)
+
+    if (not wiki_page):
+        return render(request, "encyclopedia/fail.html", {
+          "message": "Not Found",
+          "status": 404
+        })
+
+    markdowner = Markdown()
+
+    html_wiki_page = markdowner.convert(wiki_page)
+
+    return render(request, "encyclopedia/title.html", {
+        "title": title,
+        "html_page": html_wiki_page
+    })
